1fba87ed396d1c8a08e1b68e7875363b1cbbb5b9
Gunveen Kour <gunveenkour61@gmail.com>
2022-07-05 17:43:47 +0530

    modify stringparser

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
 //String->null | [JSONvalue, String]
 
-const nullparse = str => {
+const nullparser = str => {
     let result = /^null/.exec(str);
     if(result === null) return null;
     let [match] = result;
     return [match, str.slice(match.length)];
 }
-console.log(nullparse("null is null only"));
+console.log(nullparser("null is null only"));
 
 
-const booleanparse = str => {
+const booleanparser = str => {
     let result = /^true|^false/.exec(str);
     if(result === null) return null;
     let [match] = result;
     let value = match === 'true' || 'false';
     return [value, str.slice(match.length)];
 }
-console.log(booleanparse("false is wrong "));
+console.log(booleanparser("false is wrong "));
 
-const numparse = str => {
+const numparser = str => {
     let result = /[\d]+/.exec(str);
     if(result === null) return null;
     let [match] = result;
     let value = match;
     return [value, str.slice(match.length)];
 }
-console.log(numparse("one two 100"));
+console.log(numparser("one two 100"));
 
-const spaceparse = str => {
+const spaceparser = str => {
     let result = /[\s]/.exec(str);
     if(result === null) return null;
     let [match] = result;
     let value = match;
     return [value, str.slice(match.length)]
 }
-console.log(spaceparse("Be yourself"));
+console.log(spaceparser("Be yourself"));
 
 
-const stringparse = str => {
-    for(let i = 0; i <= str.length; i++){
-        if((str.startsWith('"')) && (str.endsWith('"'))){
-            return [str];
-        }
-        else return null;
-    }
-}
-console.log(stringparse('"this is string"'));
+const stringparser = str => {
+    let test1 , test2;
+       test1 = str.indexOf('"');
+       test2 = str.lastIndexOf('"')+1;   
+    if(str === null) return null;
+    let match = str.substring(test1,test2);
+    return [match, str.slice(match.length)];
+ }
+ console.log(stringparser('"hello" world'));
+ 
+ 
+       
+ 
