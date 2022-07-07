c87cff128a45ba6643a4da3bc0b606beb91d8cf9
Gunveen Kour <gunveenkour61@gmail.com>
2022-07-07 14:27:50 +0530

    Modify stringpaarser

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const nullparser = str => {
     let result = /^null/.exec(str);
-    if(result === null) return null;
+    if (result === null) return null;
     let [match] = result;
     return [match, str.slice(match.length)];
 }
@@ -11,7 +11,7 @@ console.log(nullparser("null is null only"));
 
 const booleanparser = str => {
     let result = /^true|^false/.exec(str);
-    if(result === null) return null;
+    if (result === null) return null;
     let [match] = result;
     let value = match === 'true' || 'false';
     return [value, str.slice(match.length)];
@@ -20,7 +20,7 @@ console.log(booleanparser("false is wrong "));
 
 const numparser = str => {
     let result = /[\d]+/.exec(str);
-    if(result === null) return null;
+    if (result === null) return null;
     let [match] = result;
     let value = match;
     return [value, str.slice(match.length)];
@@ -29,7 +29,7 @@ console.log(numparser("one two 100"));
 
 const spaceparser = str => {
     let result = /[\s]/.exec(str);
-    if(result === null) return null;
+    if (result === null) return null;
     let [match] = result;
     let value = match;
     return [value, str.slice(match.length)]
@@ -38,15 +38,17 @@ console.log(spaceparser("Be yourself"));
 
 
 const stringparser = str => {
-    let test1 , test2;
-       test1 = str.indexOf('"');
-       test2 = str.lastIndexOf('"')+1;   
-    if(str === null) return null;
-    let match = str.substring(test1,test2);
+    let count = (str.split('"').length - 1);
+    if (count > 2 || count === 1) return null;
+    if((str.startsWith("'")) ) return null;
+    if (str === null) return null;
+    let test1 = str.indexOf('"');
+    let test2 = str.lastIndexOf('"') + 1;
+    let match = str.substring(test1, test2);
     return [match, str.slice(match.length)];
- }
- console.log(stringparser('"hello" world'));
- 
- 
-       
- 
+}
+console.log(stringparser('"hello" world'));
+
+
+
+
